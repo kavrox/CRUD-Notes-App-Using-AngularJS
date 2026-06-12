7a22cc836b10ebb82e88484ca6db89a647d8bfa3
Anush <anushwastaken@gmail.com>
2026-06-12 18:06:06 +0530

    worked on note-editor

--- src/app/note-editor/note-editor.ts
+++ src/app/note-editor/note-editor.ts
@@ -9,13 +9,30 @@ import { NotesService } from '../notes-service';
 })
 export class NoteEditor {
   dispForm = false;
-
+  count = 0;
+  dt = new Date().toLocaleString();
   noteService = inject(NotesService);
   noteList = this.noteService.notesList;
 
   createNoteEffect = effect(
     () => {
-      this.noteService.createNewNote
+      this.noteService.createNewNote(); console.log("add btn was clicked")
+      if (this.count != 0) {
+        this.dispForm = true;
+      }
+      this.count += 1;
     } 
   )
+
+  cancelNote(){
+    this.dispForm = false;
+  }
+
+  deleteNote(){
+
+  }
+
+  saveNote(){
+    
+  }
 }
